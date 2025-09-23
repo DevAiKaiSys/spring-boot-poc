6934f20981dca87104ca1730da24fe02b3781647
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-09-23 14:17:33 +0700

    New project Spring Web, WebSocket, Spring Data Redis

---
+++ src/main/java/com/example/websocketredis/WebsocketredisApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketredisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websocketredis/WebsocketredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
